69e388da1a6151366fc52cda19a5dcfbab85d3ce
Dhirendra Singh <110583125+Dhirendra012@users.noreply.github.com>
2024-02-09 23:52:30 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,25 @@
+const buttons=Array.from(document.querySelectorAll(`button`));
+const input=document.querySelector(`.Input`);
+const output=document.querySelector(`.Output`);
+let exp="";
+function answer(){
+    // console.log(this.innerText);
+    let val=this.innerText;
+    let ans=0;
+    if(val==='AC'){
+        exp='';
+        input.innerText=``;
+        output.innerText=``;
+        return;
+    }
+    if(val==='='){
+        ans=eval(exp);
+    }
+    else{
+        exp+=val;
+    }
+    input.innerText=exp;
+    output.innerText=ans;
+    console.log(exp);
+}
+buttons.map((button)=>button.addEventListener("click",answer));
